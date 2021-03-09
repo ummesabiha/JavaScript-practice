3e10086915330c394781f79f71883a7609a8e09a
ummesabiha <umme.sabiha@mztech.us>
2021-03-09 19:17:36 +0600

    index.html, custom.js, style.css added

---
+++ custom.js
@@ -0,0 +1,17 @@
+
+var productName = "Pen"
+var price = 10;
+var quantity = 5;
+console.log(productName + price * quantity);
+
+
+console.log("My Name is JavaScript");
+let firstName = "Java";
+console.log(firstName,"Script");
+
+var pi = 3.14159;
+// pi = 123;
+console.log ("Value of pi is",pi);
+
+let pamdus = 2+5*5/4-5;
+console.log(pamdus);
